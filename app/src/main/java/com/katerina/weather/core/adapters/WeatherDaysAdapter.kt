c25a5c0f46e4f3bbbc0e21b801f5a5dbd967c067
Katerina Morozova <kattrinmr@gmail.com>
2022-11-13 21:50:07 +0300

    minor fixes

--- app/src/main/java/com/katerina/weather/core/adapters/WeatherDaysAdapter.kt
+++ app/src/main/java/com/katerina/weather/core/adapters/WeatherDaysAdapter.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.katerina.weather.R
-import com.katerina.weather.core.models.WeatherModel
 import com.katerina.weather.core.models.WeeklyForecastModel
 import com.katerina.weather.databinding.ItemDayBinding
 
